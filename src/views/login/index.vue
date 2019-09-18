e789fec5a59450a5e486b250f293e6769b990e65
lijide <1916625156@qq.com>
2019-09-18 19:51:35 +0800

    黑马头条PC-登录模块-页面布局及样式完成

---
+++ src/views/login/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="login">
+    <el-card class="login-card">
+      <div class="title">
+        <img src="../../assets/img/logo_index.png" />
+      </div>
+
+      <el-form style="margin-top: 15px">
+        <el-form-item>
+          <el-input placeholder="请输入手机号"></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <el-input style='width: 65%' placeholder="请输入验证码"></el-input>
+          <el-button style="float: right">发送验证码</el-button>
+        </el-form-item>
+
+        <el-form-item>
+          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" style="width: 100%">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang='less' scoped>
+.login {
+  background-image: url("../../assets/img/login_bg.jpg");
+  height: 100vh;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-card {
+    width: 440px;
+    height: 330px;
+    .title {
+      text-align: center;
+      img {
+        height: 45px;
+      }
+    }
+
+  }
+}
+</style>
