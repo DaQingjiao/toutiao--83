3ebe257d0b4b393c2fe8259f5532ba3a0e02590f
lijide <1916625156@qq.com>
2019-09-20 21:12:57 +0800

    黑马头条PC-主页模块-头部退出和Git地址系统完成

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -9,24 +9,63 @@
 
     <el-col :span="4" class="right">
       <div class="grid-content bg-purple">
-        <img class="head-img" src="../../assets/img/avatar.jpg" alt />
-        <el-dropdown trigger="click">
-      <span class="el-dropdown-link" style="cursor:pointer">
-        百一优<i class="el-icon-arrow-down el-icon--right"></i>
-      </span>
-      <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item>个人信息</el-dropdown-item>
-        <el-dropdown-item>Git地址</el-dropdown-item>
-        <el-dropdown-item>退出</el-dropdown-item>
-      </el-dropdown-menu>
-    </el-dropdown>
+        <!-- img 中引用的图片：-->
+        <!-- 如为固定地址，vue-cli 把固定地址转化成base64位的字符串 -->
+        <!-- 如不固定地址，就不能转成base64 -->
+        <!-- 需使用 require() 手动将图片地址转成base64 -->
+        <!-- 本地地址才需要转base64，线上地址不需要 -->
+
+        <!-- 以下判断条件得来，所以为不固定地址 -->
+        <img class="head-img" :src="userInfo.photo?userInfo.photo:defaultImg" alt />
+        <el-dropdown trigger="click" @command="commonClick">
+          <span class="el-dropdown-link" style="cursor:pointer">
+            {{userInfo.name}}
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item command='account'>个人信息</el-dropdown-item>
+            <el-dropdown-item command='git'>Git地址</el-dropdown-item>
+            <el-dropdown-item command='exit'>退出</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
       </div>
     </el-col>
   </el-row>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      userInfo: {},
+      defaultImg: require('../../assets/img/404.png')
+    }
+  },
+  methods: {
+    getuserInfo () {
+      let token = window.localStorage.getItem('user-token')
+      this.$axios({
+        url: '/user/profile',
+        headers: { Authorization: `Bearer ${token}` }
+      }).then(res => {
+        this.userInfo = res.data.data
+      })
+    },
+    commonClick (key) {
+      if (key === 'account') {
+        // 个人信息
+      } else if (key === 'git') {
+        window.location.href = 'https://github.com/DaQingjiao/toutiao--83/commits/master'
+      } else {
+        window.localStorage.clear()
+        this.$router.push('./login')
+      }
+    }
+  },
+  created () {
+    this.getuserInfo()
+  }
+}
 </script>
 
 <style lang="less" scoped>
