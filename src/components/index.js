76533ea7be851727985bd04a6cb9a88321688bb5
lijide <1916625156@qq.com>
2019-09-21 21:42:00 +0800

    黑马头条PC-面包屑组件封装完成

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Aside from './home/layout-aside'
 import Header from './home/layout-header'
+import Crumb from './common/bread-crumb'
 export default {
   install (Vue) {
     // Vue.component('组件名', 组件)
     Vue.component('layout-aside', Aside) // 注册左侧导航栏
     Vue.component('layout-header', Header) // 注册头部组件
+    Vue.component('bread-crumb', Crumb) // 注册面包屑组件
   }
 }
